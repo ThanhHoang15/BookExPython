b7be55cedb5ffc0e6c05dcff318e3dcad8a84da5
Thanh Hoang <hoang_thanh15@yahoo.com>
2026-03-02 19:21:39 -0800

    add postBook

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('postbook', views.postbook, name='postbook'),
+    path('displaybooks', views.displaybooks, name='displaybooks'),
 ]
+
--- bookMng/views.py
+++ bookMng/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import MainMenu
 
+from .forms import BookForm
+
+from django.http import HttpResponseRedirect
+
+from .models import Book
+
 # Create your views here.
 
 
@@ -17,4 +23,36 @@ def index(request):
                   'bookMng/displaybooks.html',
                   {
                       'item_list': MainMenu.objects.all()
+                  })
+
+
+
+
+def postbook(request):
+    submitted = False
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/postbook?submitted=True')
+    else:
+        form = BookForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,
+                  'bookMng/postbook.html',
+                  {
+                      'form': form,
+                      'item_list': MainMenu.objects.all(),
+                      'submitted': submitted
+                  })
+def displaybooks(request):
+    books = Book.objects.all()
+    for b in books:
+        b.pic_path = b.picture.url[14:]
+    return render(request,
+                  'bookMng/displaybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'books': books
                   })
