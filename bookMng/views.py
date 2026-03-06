4b9bf389362a6efec0af3e61455ca23e2327dc7f
Thanh Hoang <hoang_thanh15@yahoo.com>
2026-03-06 14:45:36 -0800

    add login

--- bookMng/views.py
+++ bookMng/views.py
@@ -9,6 +9,11 @@ from django.http import HttpResponseRedirect
 
 from .models import Book
 
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+
 # Create your views here.
 
 
@@ -56,3 +61,13 @@ def displaybooks(request):
                       'item_list': MainMenu.objects.all(),
                       'books': books
                   })
+
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
